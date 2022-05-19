958b9d52c203462eecfb236641c0e525f5180cd6
Vannarith Om Jr <vannarithomjr@vannarithsmbp2.lan>
2022-05-19 16:20:25 -0700

    structured-english portion done

---
+++ IS51Final.py
@@ -0,0 +1,10 @@
+"""
+This program opens a file Final.txt with the grades of students, stores the grades
+to an empty list of integers, displays the number of grades,
+the average grade, and the percent of grades that are above the average grade.
+
+These are the expectec outcomes
+Number of grades: 24 
+Average grade: 83.25 
+Percentage of grades above average: 54.17%
+"""
