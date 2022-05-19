682bd0ef84a5285532ab75290b822d64c4d7368c
Vannarith Om Jr <vannarithomjr@vannarithsmbp2.lan>
2022-05-19 16:26:04 -0700

    pseudo-code portion finished

--- IS51Final.py
+++ IS51Final.py
@@ -8,3 +8,32 @@ Number of grades: 24
 Average grade: 83.25 
 Percentage of grades above average: 54.17%
 """
+
+
+"""
+main
+    calculate_percent_above_average
+
+calculate_percent_above_average:
+    grades
+    open file
+    for loop
+        append each number to grades list
+    close file
+
+    number of grades = length of grades
+    average grade = sum of all the grades / number of grades
+    
+    set above average counter to 0
+    for loop in grades:
+        if grades[index] > average grade:
+            above average counter + above average counter + 1
+        
+    above average percentage = (above average counter / number of grades) * 100
+
+    print number of grades (24)
+    print average grade (83.25)
+    print percentage of grades before average (54.17)
+
+call main
+"""
